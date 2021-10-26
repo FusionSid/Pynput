dd3c751bf0d66c1fd26f7fe691384001bdb309b5
Siddhesh Zantye <siddheshzantye@Siddheshs-MacBook-Pro.local>
2021-10-26 20:59:41 +1300

    e

--- main.py
+++ main.py
@@ -2,18 +2,20 @@ from pynput.keyboard import Key, Controller
 import time
 keyboard = Controller()
 
-s = input("Start: ")
-e = input("End: ")
-s = int(s)
-e = int(e)
+start = input("Start: ")
+end = input("End: ")
+start = int(start)
+end = int(end)
+
 
 def count(start, end):
     while start != end:
-        time.sleep(1.5)
+        time.sleep(1)
         sta = f"{start}"
         keyboard.type(sta)
         keyboard.press(Key.enter)
         keyboard.release(Key.enter)
         start +=1
 
-count(s, e)
+time.sleep(4)
+count(start, end)
