7c57d28bb99f9dfda6a3161faa480b7d723769c8
Siddhesh Zantye <siddheshzantye@Siddheshs-MacBook-Pro.local>
2022-01-10 12:15:17 +1300

    Commit

--- Mouse/autoclicker.py
+++ Mouse/autoclicker.py
@@ -1,10 +1,56 @@
-from pynput.mouse import Controller, Button
 import time
+import threading
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Listener, KeyCode
+
+
+delay = 0.1
+button = Button.left
+start_stop_key = KeyCode(char='s') # press s to start/stop
+exit_key = KeyCode(char='e') # press e to exit
+
+
+class ClickMouse(threading.Thread):
+    def __init__(self, delay, button):
+        super(ClickMouse, self).__init__()
+        self.delay = delay
+        self.button = button
+        self.running = False
+        self.program_running = True
+
+    def start_clicking(self):
+        self.running = True
+
+    def stop_clicking(self):
+        self.running = False
+
+    def exit(self):
+        self.stop_clicking()
+        self.program_running = False
+
+    def run(self):
+        while self.program_running:
+            while self.running:
+                mouse.click(self.button)
+                time.sleep(self.delay)
+            time.sleep(0.1)
+
 
 mouse = Controller()
+click_thread = ClickMouse(delay, button)
+click_thread.start()
 
-time.sleep(4)
-for i in range(5000):
-    mouse.press(Button.left)
-    mouse.release(Button.left)
-    time.sleep(0.05)
+
+def on_press(key):
+    if key == start_stop_key:
+        if click_thread.running:
+            click_thread.stop_clicking()
+        else:
+            click_thread.start_clicking()
+    elif key == exit_key:
+        click_thread.exit()
+        listener.stop()
+
+
+with Listener(on_press=on_press) as listener:
+    listener.join()
--- Mouse/mouse.py
+++
@@ -1,9 +0,0 @@
-from pynput.mouse import Controller, Button
-import time
-
-time.sleep(3)
-mouse = Controller()
-mouse.position = (957,152)
-mouse.press(Button.left)
-time.sleep(1)
-mouse.release(Button.left)
